ca37b79d9c3b0ee453d27ec128bf8bc7a501d44d
Prakhar Rastogi <51193428+prakharR534@users.noreply.github.com>
2019-09-28 23:14:34 +0530

    Add files via upload

---
+++ intro.py
@@ -0,0 +1,12 @@
+import cv2
+
+img = cv2.imread('pic.jpg',0)
+cv2.imshow('image', img)
+
+k= cv2.waitKey(0)
+
+if k == 27:
+	cv2.destroyAllWindows()
+elif k== ord('s'):
+    cv2.imwrite('pic-copy',img)
+    cv2.destroyAllWindows()
